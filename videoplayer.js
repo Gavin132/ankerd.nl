e261a41be456938b038564713726ec3b87285b59
gavin132_ <75799630+Gavin132@users.noreply.github.com>
2025-05-10 00:11:46 +0200

    video player progress

--- videoplayer.js
+++ videoplayer.js
@@ -1,12 +1,6 @@
-const playbutton = document.getElementById('playBtn');
-const videoModal = document.getElementById('videoModal');
-const modalVideo = document.getElementById('modalVideo');
-const closeBtn = document.getElementById('closeBtn');
-
 playbutton.addEventListener('click', () => {
-  background-video-popout.style.display = 'flex';
-});
-
-closebutton.addEventListener('click', () => {
-  background-video-popout.style.display = 'none';
+  const backgroundVideoPopout = document.getElementById('background-video-popout');
+  const fullscreenVideoOverlay = document.getElementById('fullscreen-video-overlay');
+  backgroundVideoPopout.style.display = 'flex';
+  fullscreenVideoOverlay.style.display = 'flex';
 });
